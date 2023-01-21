6f2fd464030b61fb6ad6e1a3843da60412f5e34b
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-21 11:14:06 +0000

    Implemented testing for validity of non-final rounds

--- spec/score_calculator_spec.rb
+++ spec/score_calculator_spec.rb
@@ -1,12 +1,13 @@
+require_relative "../score_calculator"
+
 describe ScoreCalculator do
 
   let(:score_calculator) { ScoreCalculator.new }
 
   before(:each) do
-    @test_scores = [
+    @scorecard = [
       [1, 4],
       [4, 5],
-      [8, 1]
       [6, 4],
       [5, 5],
       [10, 0],
@@ -18,11 +19,34 @@ describe ScoreCalculator do
     ]
   end
 
-  context "#calculate accepts only valid bowling inputs" do
-    it "returns string unless input has 10 rounds" do
-      @test_scores << [1, 7]
-      expect(score_calculator.calculate(@test_scores)).to eq "Sorry, your input has an invalid number of rounds"
-      expect(score_calculator.calculate(@test_scores[0...9])).to eq "Sorry, your input has an invalid number of rounds"
+
+  it "returns an integer for valid scorecard" do
+    expect(score_calculator.calculate(@scorecard)).to be_a Integer
+  end
+
+  it "returns string unless input has 10 rounds" do
+    @scorecard << [1, 7]
+    expect(score_calculator.calculate(@scorecard)).to eq "Sorry, your input has an invalid number of rounds"
+    expect(score_calculator.calculate(@scorecard[0...9])).to eq "Sorry, your input has an invalid number of rounds"
+  end
+
+  context "When checking non-final rounds" do
+
+    it "returns string unless each round has two entries" do
+      @scorecard[4].pop
+      expect(score_calculator.calculate(@scorecard)).to eq "Sorry, one of your non-final round scores is invalid"
+      @scorecard[4] << 0 << 0
+      expect(score_calculator.calculate(@scorecard)).to eq "Sorry, one of your non-final round scores is invalid"
+    end
+
+    it "returns string unless sum of round entries is <= 10" do
+      @scorecard[0][0] = 7
+      expect(score_calculator.calculate(@scorecard)).to eq "Sorry, one of your non-final round scores is invalid"
+    end
+
+    it "returns string unless all entries are non-negative integers" do
+      @scorecard[6][1] = -1
+      expect(score_calculator.calculate(@scorecard)).to eq "Sorry, one of your non-final round scores is invalid"
     end
   end
 end
